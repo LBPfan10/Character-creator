3b666376acd9c1f8e62e6c8f94af4019eaa291c7
LBPfan10 <157020566+LBPfan10@users.noreply.github.com>
2024-04-16 11:49:32 +0100

    Add files via upload

---
+++ assignment 2- interactive page/assignment 2- interactive page/Code/code.js
@@ -0,0 +1,38 @@
+function createcharacter(){
+    let nameInput = document.querySelector('#PlayernameInput');
+let nameOutput = document.querySelector('#PlayernameInput');
+
+let birthdayInput = document.querySelector('#birthday');
+let birthdayOutput = document.querySelector('#dob');
+
+let jobInput = document.querySelector('#class');
+let jobOutput = document.querySelector('#class')
+
+let levelInput = document.querySelector('#level');
+let levelOutput = document.querySelector('#currentLevel');
+
+let strengthInput = document.querySelector('#strength');
+let strengthOutput = document.querySelector('#currentStrength');
+
+let dexterityInput = document.querySelector('#dexterity');
+let dexterityOutput = document.querySelector('#currentDexterity');
+
+let constitutionInput = document.querySelector('#constitution');
+let constitutionOutput = document.querySelector('#currentConstitution');
+
+let inteligenceInput = document.querySelector('#inteligence');
+let inteligenceOutput = document.querySelector('#currentInteligence');
+
+let wisdomInput = document.querySelector('#wisdom');
+let wisdomOutput = document.querySelector('#currentWisdom');
+
+let charismaInput = document.querySelector('#charisma');
+let charismaOutput = document.querySelector('#currentCharisma');
+
+
+let good = document.querySelector('#good');
+let neutral = document.querySelector('#neutral');
+let evil = document.querySelector('#evil');
+let AlignmentOutput = document.querySelector('#alignment');
+
+}
